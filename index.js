7977b69fb8e6e082f2bc1e3572defb4ecd6e6fed
Taras Tarlov <ttarlov@gmail.com>
2020-07-25 11:23:22 -0600

    Paste repalcement code

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
+const express = require('express');
+const app = express();
+
+
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: false });
+let nightmare = Nightmare({ show: true });
+//run npm start or nodemon (if you have nodemon installed globally)
+// to run the app locally
+//this sets the post at 3001 - go to localhost:3001 in your browser to see app
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Twitter Hackery';
+// at localhost:3001/, you should see the text in this response
 
-const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
-console.log('Welcome to Nightmare scrape\n==========');
+app.get('/send-tweet', async (request, response) => {
+    var message = request.param('message');
+    var handle = request.param('handle');
+    try {
+    var tweet = await sendTweet(message, handle);
+    return response.json(tweet);
+    } catch (err) {
+    console.log(err);
+    }
+});
 
-nightmare
-    .goto(URL)
-    .wait('.post-title')
-    .evaluate(() => document.querySelector('.post-title').textContent)
-    .end()
-    .then((result) => {
-        console.log(result);
-        console.log('=========\nAll done');
+app.listen(app.get('port'), () => {
+
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+app.get('/', (request, response) => {
+    response.send('Tweeter Hackery');
+});
+function sendTweet(message, handle) {
+    var url = `https://twitter.com/intent/tweet?text=${message}&via=${handle}`;
+    return nightmare
+    .goto(url)
+    .wait(500)
+    .type('input[type="text"]', 'oversiteoversite@gmail.com')
+    .type('input[type="password"]', 'oversite123')
+    .type('input[type="password"]', '\u000d')
+    .wait(4500)
+    .evaluate(() => {
+      let tweetBtn = [...document.querySelectorAll('span')].find(span => span.innerText === "Tweet")
+      tweetBtn.addEventListener("click", e => { console.log("clicked") })
+    	tweetBtn.click()
     })
-    .catch((error) => {
-        console.error('an error has occurred: ' + error);
+    .wait(2000)
+    .then(() => {
+      	return nightmare.end();
     })
-    .then(() => (console.log('process exit'), process.exit()));
+    .then(function(){
+    nightmare  = Nightmare()
+    	console.log("nightmare done")
+    })
+}
